b756bddecb1011a2b69c78b5b19e9440d118edf3
12MAnWA <134224432+12MAnWA@users.noreply.github.com>
2026-09-19 18:48:02 +0300

    Delete Day 2-Gmail users who registered but never used-for a market campaign.sql

--- Day 2-Gmail users who registered but never used-for a market campaign.sql
+++
@@ -1,8 +0,0 @@
---project: Marketing wants Gmail users who registered but NEVER ordered to re-engage them
--- Uses: WHERE + LIKE + LEFT JOIN + IS NULL
-
-SELECT customers.name, customers.id
-FROM customers
-LEFT JOIN orders ON customers.id = orders.customer_id
-WHERE customers.name LIKE '%gmail%' -- if you have email column, use email
-AND orders.id IS NULL;
