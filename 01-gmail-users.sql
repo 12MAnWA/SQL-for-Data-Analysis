e2bb597de684ef8edb8c3cc442e7b8c10c0418a5
12MAnWA <134224432+12MAnWA@users.noreply.github.com>
2026-09-22 21:50:54 +0300

    Delete 01-gmail-users.sql

--- 01-gmail-users.sql
+++
@@ -1,8 +0,0 @@
--- DAY 1: Find all customers using gmail
--- Business use: For targeted email campaign
-
--- Table: Customers(id, name, email)
-
-SELECT id, name, email
-FROM Customers
-WHERE email LIKE '%gmail.com';
